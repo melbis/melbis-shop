c5e2ee42b121e8ddd215e233d2fd3c4dbc00937a
melbis <dima@kasyanoff.com>
2025-04-28 12:01:49 +0000

    v6.4.0.90 2025-04-28 12:01:49

--- core/core.php
+++ core/core.php
@@ -1,20 +1,21 @@
 <?php //00300
-// Melbis Shop v6.4.0.89 Copyright melbis.com 2025
+// Melbis Shop v6.4.0.90 Copyright melbis.com 2025
 if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
 ?>
-HR+cPq6i9ETJY9t8AXQnnFYkaFs0dyjb3uVEHvsu9+IwuTZkoKtWJUwPcw8+WiB0f99VqAgEAliB
-68A38DuQFeqqG9bxVlcFpITXRI+RclrZNlXqubJXf15Ma/kvpBv2eWNKVJOx90vwiwom7isGe97y
-mlwxf9IAsgCEAt6lyxWJNG7bH3HkrMnGP28efWuN/lVFkU0B94Mrvd/GAHiPX7jsk7C7poXbh/PZ
-nsseY4frw7tOsBFCEgcnCKRrLb/I9lkZfGa10gwAx9uRcG0LQiMzdPNBN8jk4BSr8T2DJAKMoWxa
-114c/r7bSXiVNOZ2wd5nloBywOkJD8AJavi9iOuSEDupKtfmO2GA+KPXOlgpYH19HXdJwA0PtmaD
-CqphMdxCll0i1nsItBBmGDCqs7UNV038er4SoRsfWvSw97rkadAbz3fLlzsg7btBi5vKqo8AKE/I
-+/ziT5y9S73pyBHFcnOUWnIzhT1KbI3QtELX3uvfYW4BlVANN/HGe57YryVMWahF7FwUccNAsN2h
-A773LMc3+Bhij+tIloNik948D1UFc+pVQPn51/6xlMXw8h3SHHAQbXvcvHJebUlOvk2cCm5O4IGW
-mOtLE0NOtE62j+ZAd8vBqA1jY/98yhU1Bi81JOYbZtvKfIAY5ULgHAEqDzP6EJGgGsShkcoWUUfJ
-GS+q6pus9TFDyFoneqvrzkloYzFyDaM5twKZ8dtmk8+sn4lN1zCnpAFWQBwXzxVe9WkbSXyVQEY4
-8dwKdezygiZ2mrMjuTzReyABsVHKG684aF9yR7CdQCOVeBEMjCx9rC9a5XQilZxzkrRnlPHMk61j
-8axH1H57Sg+ULTIQ3WEFztHM4vmpbOeeB4mCJ6lpKxAcl9FpTwDd0Zs7Ijp6qMkLZ9UB6yvdyabU
-b+D1ppUEGWhlwBhCou5lIcWTWN6Ddzljd8X+o8E0owQYWq6V2Av0Urw7gscmosDj0VDmmYvyc8Y5
-JSjClreJI2+qDX/TqIf6lgDpQL8eoiV8oJ02oycBzaXWvlfGOxlMFu2jXV4zhnscoyIHAPTRNeZA
-FMiqp2qNqW1r7ylQHz0l5DhbMbUDl8jwfKp/60yswNOk1tT5wpWo8QgD3Gzpic0amlJThv5hzDqw
-+s6sk0zdd/v5HMGa26F4gSwer8xrELP/D/feX+YvZOHDxOgX+OG7IUbVueQYRk/QioKSmQLYO8Sg
+HR+cP/XCdyR3g6FfclOAvJd5iTaiaJeRAXXTeD68yZraFdYJNTsHj/HYdKA6cGOjIsaj7HwJd+HN
+uhPYRRO+UiGQs4CCYaf7d8/h6awFbbS6MfH8/tG24z7WE15RAJ7y7Xjb3SzifBGdnpXiy3blXCc/
+gwFd77OM/Y5XeB4vaYY+EqmLiWgpjwZVPucBqNDn7qmJdT0cZytHCE8YeBbgUYO9rfrEsv0lURXC
+mkWauOGLojrclE46lpuP4GMsXZiHkNWAKW+CtkJbe1rznbb7S8O+EFjndLNLP1z9c76Vvv2yIDZD
+5UEFDF+VVcwuNEA0Mp8G9N7FAg6ljwUIhMKbShJJSPYQj2QhulDS9a3gZnjJ+XpGIVuq+YY/RULX
+a4ErWU+pJ3sR4/u2gno/jnx/pqju5apTl0FHsba8YAkwCWNf8wFjTP15vcwzCgEIcrfNFPCQKTPw
+Yvhbk2iDtVMj8xZvGSEXrr/TbknmRla8yr5IwlXquQTtTiIGo2g5ycDgeJcZghZi4s2oQtrVrUF+
+kd0byr2E+XmFre2+SrJhQW/nxgzq6HbjoparTHMArQHJI8uMLpehhatzgdd3/Eo9Jt7ApQ4hw7gy
+IX83f5J4gw//c8wljzwVymXnyYxT40oQCYN9tUewk51qPsd3q4qLiRmKe6J6AfT2hywlSZAi8NYS
+yPLUPTDpdykUA9Lv86C8olQk62Fhc7WZfWkH4VrUO7e8Uk7C6dXNfDMG+DggRvE8Nl5EsEXhGG0R
+JEgUhDPYtxFKyK00h40qwXZOQO/WljsKV7INEJstlQjh0J8S1mY0jf+YUpWYRgH+UR7I3LcG+uro
+dqDAzDwTJyAiwZDWqUUlX9q/5z3M6Ozy82t6UCORKLlsbfV1P54aK15i8lOYjmy7E2ThJDsVR+Po
+jO6qg6gffM+wHnSmH9jaHlHNVUwC3Yc51tlt7S05rP0S9AGrI9X27VxxUKDCtD63U9roVHOhYFj+
+TUMVM8UDzLqVdFWO/X/jJyw0zOjfmud3oIigk3xpnsRtldiZ9GSBPvsIAWpNCoEjsn+maBD5s5UA
+/YuY4/5dND5YGW5+yfhVwHZJr9FWn2kABo+DzDqsAZWja0cqY8u4NqjzlkGOexNOzvlsNRFLsVHV
+D0L6ZLXdhJs8R8S7/F1IofTzNjuKO14RnPbnY4R4tTQf/BPCPbA11CN5FiT5/Ocf43ZfEfXlvavm
+lykxjLyrf0==
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Melbis Shop 6 Server Core
+
+Please refer to the installation instructions at: https://melbis.com/en/dev/install/
+
+Пожалуйста, ознакомьтесь с инструкцией по установке по адресу: https://melbis.com/ru/dev/install/
+
+Будь ласка, ознайомтеся з інструкцією з встановлення за адресою: https://melbis.com/uk/dev/install/
