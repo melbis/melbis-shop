e99e93fd8e03b916c5f1658781fcdd817c3bc6b5
melbis <dima@kasyanoff.com>
2026-04-03 16:06:43 +0100

    v6.5.0.208 2026-04-03 16:06:43

--- core/core.php
+++ core/core.php
@@ -1,24 +1,24 @@
 <?php //00313
-// Melbis Shop v6.5.0.207 @ 2026-04-03 Copyright melbis.com 2002-2026
+// Melbis Shop v6.5.0.208 @ 2026-04-03 Copyright melbis.com 2002-2026
 if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
 ?>
-HR+cPq45Fnn+q8DJ1xwgFyOAytnWJNvr/ff9KOgu3iT68MiNg1/ou0X+z6KHR6seHtfzLb8m0BWM
-dDk3FLZ36nMuRWQfYMrRaEuOMBZsrsKg98vL+LzESebyaFwJjTd6gP0ilx4ttcFz0SXdT35Cu65g
-IrIbBwp++itIPkbuJsAxSbBOVAAubdKtVDvdzEoWTbw40eBdlVzUWmvYik0dWW4wUwCqMxbjakKi
-BP14BsLnP9uPvvwo9AyFeslFys5+v9O+qlo23/6js0w5NWA4AsMiIqOGYyjc7dy2Q+f8jCvDUie9
-0YHn/xu4GkdcnmMP1wfvG8u3Ov4g1bZxRBbJDLTYQz0M9AdDYLzQSxX0fKWxiF1YQeFI8c6BoekF
-FS9MUHl0xgty7XVEotxsXJiLAQj/w6kalLq+ljymsc0SC4c6lIxHlsudDohR0cLlnziUH2yLbZyd
-7j5IjiDSw7DxcMjzO7jfAdR9Qw9szFciDVhRmHOX0eM/5R8RyACc79T9nkNB61Mic5ky1VI1rBWD
-dsni8JP0kFZu+KGftHFzsmbwKuB5BfCaQ1hxyoUyPpQx4xkeLiQwXj9nTptZFsZo2tlw+uX7s298
-C2bXa74anK20WlDS8l6QhVh75netNzlfdBnDZhvu8ZL06EgqSaIEPu3yrfJRXGztr9fLq57kb2v3
-i7580Oeh4TB7hlxZwb8sEH57fR09w8DYGMOuRPIbfzjhizPcPUBkqPN3AxwwdWF+FVBr8qyvkXPi
-YBtHNnCwgceSUTVzqHpFcWLkxkjaZxE/G+XvkHUTgEdpNJGhHxyB0QzXOpxEYdFEkqYRRuVPf9Qh
-Z2a/cgO7+VPfQZujWHfOOw5VcQiNkncdlx3LWoYDJ6ncosv7LajAVV7hf4MRGfOYhuf8E4K1qpAS
-9ihjT7wRL4TXMeIpd3MiG9AKdLGrtoAHV8FGarGa9tUpwcrabR6gyBBBvsqVLKaE9DfbgEHvDLJy
-vMJIXOI2FnTjvNcDQDjFACnKpxjuG79mLXXrpzaHav+lJ+T9JlGUi9ZVvwu+Ks/5uBjjfSqDJrzj
-MwGC07zzYYElHyA9deTTn67B0F5Wj6MLE1C1M4jlVpXpSsn7D691n8Rx2IOdXXTMhOiu1ZgNHhX8
-21L6LnEdHJieGBanrGdDKKRAmasSXRZyX97ONjTVviY/JCCCj8jDLCLiL7NBhIuABk/AOGQohemN
-u95zRCbCyyP82uhF7KgrFb2zqvB98sibegoqT7r3WvQJS4mphSZ5yFEISU/QTtKF7pZQhpZmc3Ux
-IP+3O0rs4Fa4mef3/QRda1N+HGCkD8sPJjCvdK3dzI6Xmi91/wfOLfYyRptHSwHMOewPzIHc6Lms
-PlSCjsZTW/138Yn5oiJZiVCcNH7p+2/MTIbgakHR2LP4DRebgnunY+zVKc3mpaILHl3tK5WToAXD
-NrKGHOFx1Yh5HOppc7KJ4CAiSaXUZ/e3o1mH3gdxfqwYZxCCvm==
+HR+cPnbFvR68gTs6LVHmaiUCupJgRBO4WcYnkEfP3JK3XHpjwQjWgKdXXjXGl3JE8OGtagNNYJAU
+r2xn91uIwnbU0Cdtp8qmc04P269wiRHZu0y26lmimksVTv09S6Xon80mfqu5YiWB1wA0aTeQm+m2
+diqJkvg5NgKsYUUZa7Z5KcYABfOiMk9mGUrmg+azhSvRz8Hn27AHTwhC3zYEEgPtDCFQnIPw1Adj
+DC40j11vKuCZHaq2EQZ1v51CNB64mCaClZBci0OYFjzmmTe1hlKBO6ixSpViPlEDubnXiWJX74DD
+suPyUl/QBw/Xy3ytnjxxAcnePeArC0yBwqMyAmSJ8jaNwqFNAdC/hG5aRoUj/YgzIXNnx6PS+dZi
+YF2/53EcrfU0sGJ7FqzijT2er7WeFeYkfpgomkIObOQ+blkE/nYn888AjxOEyIQwcS8rzu+S+4a/
+q9j6yceAeilm0cHm9rXW2KigG5oH2JeOi4qeHcfVJSgUmTAwzk59VGAUpHSsEZ+iItZqKejAxAYt
+6wAO7oOmbDO192OohphgoHJvOXVNOWDKNmvV1ylJPZ0EziozbInWhVDouthPcdngaMyI15xHuCU/
++wuWUdeuHfmgV5z5dg6oFzRPf/yrNIRWddyTsamWhSTGZaU2uRvAiJAC6xhADF9iTzx8GjD05ZQ5
+fWBa8x4818sICDP67/dNTQAxCUHVLv65BrddDXdEOKHzoxRUHKQ8K9zwVfYAccvOkN+uDlItPtFB
+ELGtJg8ILUNX3hYcDRZ1jTUik5nYqvywBSDBhAB+xgnZeOsZ+DwJiRt0+ooKEe+7aY2EDCblfZ3M
+FLOIHgAGctvmHMQss252HjFUKeD3dDn9n2F7XXUdG3H/9WhK9GxaN3iutz0uQsSlM7Ds6mTOGpGm
+5SI3m7u0mWrFb7cy1IyQ/ilN/7b2D1Rn2rHLd5HSxoQqzTio6rRco5pqsD/XqtqeV38n2RkLQq4o
+c4TuvgauipF/4lEsP1zut6PkBgHf1jZj3Ad97IZ3KKqnxk9PSXiPemBDAMUBFvHA6XPzr1sD8MPh
++FBsge39lTV4hPy9JbIE3ZducR7q2NVJgQ9BQxp/N4/6dzaKHrlwLA3Tq/xtLxRrn7RhVs3tKbxB
+yYfNbgdZQrRGAZjMDkmlayTb4VOI4tGP+zs0TNPI4REYTGEyAXU+/NUM1JkGbsHZh9VQruN1mfMw
+ZERlw2kFXV7LosP/jDtli7T+SQuGRftWMHoH+alMtoLXArD0AS/YZtiohawZUNrHeQpStssRVFVB
+LGFrihA5jn9FngLzfFngQgAauz/Rv9cz5Bi3ZaftwPbejQTN9MPYWD5zDNNhaxPf1yI2zN57pYWJ
+jlIywFAWMwQnWDmhqxwANyDI5eZf6wWRTEZYY/Nq0bt8GikABzQDNzTayzM82Z6B1ekw8ziRf9fs
+7u+1vmuuJCbnIDw0+UF4V4Wf3Cm7iy4oyUwonhyX50==
